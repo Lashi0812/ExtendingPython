f3c02f99a1d5a4eb9c2204df4561f064aa5169d0
Lashi <lashi0812@gmail.com>
2024-01-30 17:45:05 +0000

    ndarray

---
+++ ext_with_nanobind/narray.cc
@@ -0,0 +1,46 @@
+#include "nanobind/nanobind.h"
+#include "nanobind/ndarray.h"
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+
+namespace nb = nanobind;
+
+const float data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+NB_MODULE(narray, m) {
+    m.def("inspect", [](nb::ndarray<> a) {
+        printf("Array data pointer : %p\n", a.data());
+        printf("Array dimension : %zu\n", a.ndim());
+        for (size_t i = 0; i < a.ndim(); ++i) {
+            printf("Array shape [%zu] : %zu\n", i, a.shape(i));
+            printf("Array stride [%zu] : %zu\n", i, a.stride(i));
+        }
+        printf(
+          "Device Id = %u (cpu=%i,gpu = %i)\n",
+          a.device_id(),
+          int(a.device_type() == nb::device::cpu::value),
+          int(a.device_type() == nb::device::cuda::value));
+        printf(
+          "Array dtype : int16=%i , uint32=%i ,float=%i\n",
+          int(a.dtype() == nb::dtype<int16_t>()),
+          int(a.dtype() == nb::dtype<uint32_t>()),
+          int(a.dtype() == nb::dtype<float>()));
+    });
+
+    m.def(
+      "cpu_process", [](nb::ndarray<uint8_t, nb::shape<nb::any, nb::any>, nb::device::cpu> data) {
+          data(0, 0) = (uint8_t)data(0, 0) * 5;
+      });
+
+    m.def(
+      "gpu_process", [](nb::ndarray<uint8_t, nb::shape<nb::any, nb::any>, nb::device::cuda> data) {
+          // data(0, 0) = (uint8_t)data(0, 0) * 5;
+      });
+
+    m.def("ret_numpy", []() {
+        size_t shape[2] = {2, 4};
+        return nb::ndarray<nb::pytorch, const float, nb::shape<2, nb::any>>(
+          data, /* ndim = */ 2, shape);
+    });
+}
